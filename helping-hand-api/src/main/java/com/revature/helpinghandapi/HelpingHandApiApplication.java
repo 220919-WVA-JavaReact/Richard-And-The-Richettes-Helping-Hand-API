bb28c49c029be37be5224b376926a044ce39d371
sam <ortegachan867@gmail.com>
2022-11-02 15:36:32 -0400

    added project starter

---
+++ helping-hand-api/src/main/java/com/revature/helpinghandapi/HelpingHandApiApplication.java
@@ -0,0 +1,13 @@
+package com.revature.helpinghandapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpingHandApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpingHandApiApplication.class, args);
+	}
+
+}
---
+++ helping-hand-api/src/test/java/com/revature/helpinghandapi/HelpingHandApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.helpinghandapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpingHandApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
