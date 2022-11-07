4c32ab8a04370c284cf847b7158e27548aef499f
sam <ortegachan867@gmail.com>
2022-11-07 15:27:57 -0500

    changed enpoint for client register

--- helping-hand-api/src/main/java/com/revature/helpinghandapi/controllers/ClientController.java
+++ helping-hand-api/src/main/java/com/revature/helpinghandapi/controllers/ClientController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/client")
 public class ClientController {
 
     private final ClientService cs;
