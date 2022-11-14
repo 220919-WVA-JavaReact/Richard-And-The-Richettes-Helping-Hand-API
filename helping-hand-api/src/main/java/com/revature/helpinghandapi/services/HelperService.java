7402b8e409017cf05b91632bd4ad4a736d15e301
sam <ortegachan867@gmail.com>
2022-11-14 16:50:04 -0500

    added register exception

--- helping-hand-api/src/main/java/com/revature/helpinghandapi/services/HelperService.java
+++ helping-hand-api/src/main/java/com/revature/helpinghandapi/services/HelperService.java
@@ -3,6 +3,7 @@ import com.revature.helpinghandapi.dtos.Credentials;
 import com.revature.helpinghandapi.dtos.HelperDTO;
 import com.revature.helpinghandapi.entities.Helper;
 import com.revature.helpinghandapi.exceptions.LoginException;
+import com.revature.helpinghandapi.exceptions.RegisterException;
 import com.revature.helpinghandapi.repositories.HelperRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,7 +19,7 @@ public class HelperService {
 
     public HelperDTO createHelper(Credentials cred){
         if(hr.getHelperByUsername(cred.getUsername()).isPresent()){
-            // adding validation later
+            throw new RegisterException();
         }
         Helper newHelper = new Helper();
         newHelper.setFirst(cred.getFirst());
@@ -31,7 +32,7 @@ public class HelperService {
     public HelperDTO authenticate(Credentials cred) {
         Helper helper = hr.getHelperByUsernameAndPassword(cred.getUsername(), cred.getPassword()).orElseThrow(LoginException::new);
         if(cred.getUsername() != helper.getUsername() || cred.getPassword() != helper.getPassword()){
-            // adding validation later
+            throw new LoginException();
         }
         return new HelperDTO(helper);
     }
