a3c071f30a2abeffc61720533ae959d7a1d726e9
MorganMr47 <morgan.mcgrath007@gmail.com>
2022-11-15 01:06:58 -0600

    added two more tests, still working on getAllBids

--- helping-hand-api/src/test/java/com/revature/helpinghandapi/services/BidTests.java
+++ helping-hand-api/src/test/java/com/revature/helpinghandapi/services/BidTests.java
@@ -11,17 +11,16 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
-
 import static com.revature.helpinghandapi.entities.Status.PENDING;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 
 @SpringBootTest(classes= HelpingHandApiApplication.class)
-public class BidTests{
+public class BidTests {
 
     @MockBean
     private BidRepository br;
@@ -36,7 +35,7 @@ public class BidTests{
     private BidService bs;
 
     @Test
-    public void createBidTest(){
+    public void createBidTest() {
         Client client = new Client();
 
         Date date = new Date();
@@ -68,7 +67,6 @@ public class BidTests{
         expectedBidDTO.setRequest(request);
 
 
-
         Mockito.when(rr.findById(request.getId())).thenReturn(Optional.of(request));
         Mockito.when(hr.findById(helper.getId())).thenReturn(Optional.of(helper));
         BidDTO actual = bs.createBid(bidDTO);
@@ -79,7 +77,7 @@ public class BidTests{
     }
 
     @Test
-    public void getBidByIdExists() {
+    public void getBidByRequestIdTest() {
         Client client = new Client();
 
         Date date = new Date();
@@ -91,6 +89,13 @@ public class BidTests{
         helper.setFirst("HelperF");
         helper.setLast("HelperL");
 
+        Helper helper2 = new Helper();
+        helper2.setId("2");
+        helper2.setUsername("user2");
+        helper2.setPassword("pass2");
+        helper2.setFirst("first2");
+        helper2.setLast("last2");
+
         Request request = new Request();
         request.setId("exampleRequest");
         request.setClient(client);
@@ -98,11 +103,40 @@ public class BidTests{
         request.setDeadline(date);
         request.setAvailability(Availability.OPEN);
 
-        BidDTO bidDTO = new BidDTO();
-        bidDTO.setAmount(250);
-        bidDTO.setRequest(request);
-        bidDTO.setHelperId(helper.getId());
+        Bid bid = new Bid();
+        bid.setAmount(250);
+        bid.setRequest(request);
+        bid.setHelper(helper);
+        bid.setStatus(PENDING);
 
+        Bid bid2 = new Bid();
+        bid2.setAmount(200);
+        bid2.setRequest(request);
+        bid2.setHelper(helper2);
+        bid2.setStatus(PENDING);
+
+        BidDTO bidDTO = new BidDTO();
+        bidDTO.setRequest(request);
+        bidDTO.setAmount(250);
+        bidDTO.setHelperId(helper.getId());
+        bidDTO.setStatus(PENDING);
+
+        BidDTO bidDTO2 = new BidDTO();
+        bidDTO2.setRequest(request);
+        bidDTO2.setAmount(200);
+        bidDTO2.setHelperId(helper2.getId());
+        bidDTO2.setStatus(PENDING);
+
+        List<BidDTO> bids = new ArrayList<>();
+        bids.add(bidDTO);
+        bids.add(bidDTO2);
+
+        Mockito.when(br.findByRequestId("exampleRequest")).thenReturn(bids);
+
+        List<BidDTO> actual = bs.getBidsByRequestId("exampleRequest");
+        assertEquals(actual, bids);
+        System.out.println("Expected: " + bids);
+        System.out.println("Actual: " + actual);
 
 
 //        assertEquals(expected, actual);
@@ -138,14 +172,133 @@ public class BidTests{
 
     }
 
-//    @Test
-//    public void getBidsByHelperId() {
-//        Helper helper = new Helper();
-//        Bid bid = new Bid();
-//
-//        helper.setId("exampleRequest");
-//
-//        Mockito.when(br.findById("exampleRequest")).thenReturn(Optional.of(bid));
-//        BidDTO expected
-//    }
+    @Test
+    public void getBidByHelperIdTest() {
+        Client client = new Client();
+
+        Date date = new Date();
+
+        Helper helper = new Helper();
+        helper.setId("exampleHelper");
+        helper.setUsername("ExampleUser");
+        helper.setPassword("ExamplePass");
+        helper.setFirst("HelperF");
+        helper.setLast("HelperL");
+
+
+        Request request = new Request();
+        request.setId("1");
+        request.setClient(client);
+        request.setTitle("title1");
+        request.setDeadline(date);
+        request.setAvailability(Availability.OPEN);
+
+        Request request2 = new Request();
+        request2.setId("2");
+        request2.setClient(client);
+        request2.setTitle("title2");
+        request2.setDeadline(date);
+        request2.setAvailability(Availability.OPEN);
+
+        Bid bid = new Bid();
+        bid.setAmount(250);
+        bid.setRequest(request);
+        bid.setHelper(helper);
+        bid.setStatus(PENDING);
+
+        Bid bid2 = new Bid();
+        bid2.setAmount(200);
+        bid2.setRequest(request2);
+        bid2.setHelper(helper);
+        bid2.setStatus(PENDING);
+
+        BidDTO bidDTO = new BidDTO();
+        bidDTO.setRequest(request);
+        bidDTO.setAmount(250);
+        bidDTO.setHelperId(helper.getId());
+        bidDTO.setStatus(PENDING);
+
+        BidDTO bidDTO2 = new BidDTO();
+        bidDTO2.setRequest(request2);
+        bidDTO2.setAmount(200);
+        bidDTO2.setHelperId(helper.getId());
+        bidDTO2.setStatus(PENDING);
+
+        List<BidDTO> bids = new ArrayList<>();
+        bids.add(bidDTO);
+        bids.add(bidDTO2);
+
+        Mockito.when(br.findByHelperId("exampleHelper")).thenReturn(bids);
+
+        List<BidDTO> actual = bs.getBidsByHelperId("exampleHelper");
+        assertEquals(actual, bids);
+        System.out.println("Expected: " + bids);
+        System.out.println("Actual: " + actual);
+    }
+
+    @Test
+    public void getAllBids() {
+        Client client = new Client();
+
+        Date date = new Date();
+
+        Helper helper = new Helper();
+        helper.setId("exampleHelper");
+        helper.setUsername("ExampleUser");
+        helper.setPassword("ExamplePass");
+        helper.setFirst("HelperF");
+        helper.setLast("HelperL");
+
+
+        Request request = new Request();
+        request.setClient(client);
+        request.setTitle("title1");
+        request.setDeadline(date);
+        request.setAvailability(Availability.OPEN);
+
+        Request request2 = new Request();
+        request2.setClient(client);
+        request2.setTitle("title2");
+        request2.setDeadline(date);
+        request2.setAvailability(Availability.OPEN);
+
+        Bid bid = new Bid();
+        bid.setAmount(250);
+        bid.setRequest(request);
+        bid.setHelper(helper);
+        bid.setStatus(PENDING);
+
+        Bid bid2 = new Bid();
+        bid2.setAmount(200);
+        bid2.setRequest(request2);
+        bid2.setHelper(helper);
+        bid2.setStatus(PENDING);
+
+        BidDTO bidDTO = new BidDTO();
+        bidDTO.setRequest(request);
+        bidDTO.setAmount(250);
+        bidDTO.setHelperId(helper.getId());
+        bidDTO.setStatus(PENDING);
+
+        BidDTO bidDTO2 = new BidDTO();
+        bidDTO2.setRequest(request2);
+        bidDTO2.setAmount(200);
+        bidDTO2.setHelperId(helper.getId());
+        bidDTO2.setStatus(PENDING);
+
+        List<Bid> bids = new ArrayList<>();
+        bids.add(bid);
+        bids.add(bid2);
+
+        List<BidDTO> bidDTOS = new ArrayList<>();
+        bidDTOS.add(bidDTO);
+        bidDTOS.add(bidDTO2);
+
+        Mockito.when(br.findAll()).thenReturn(bids);
+
+        List<BidDTO> actual = bs.getAllBids();
+        assertEquals(actual, bidDTOS);
+        System.out.println("Expected: " + bids);
+        System.out.println("Actual: " + actual);
+    }
 }
